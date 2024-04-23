318fc922e0d0d5214ea461991b4d955c406ef490
ST-Vatsal-Shah <120327870+ST-Vatsal-Shah@users.noreply.github.com>
2024-04-23 12:48:57 +0530

    Appcmlo

--- src/Customer/pages/HomePage/HomePage.jsx
+++ src/Customer/pages/HomePage/HomePage.jsx
@@ -3,18 +3,19 @@ import MainCarousel from "../../components/HomeCarousel/MainCarousel";
 import HomeSectionCarousel from "../../components/HomeSectionCarousel/HomeSectionCarousel";
 import { mens_kurta } from "../../../Data/mens_kurta";
 import { mainCarouselData } from "../../components/HomeCarousel/MainCarouselData";
+import { sareePage1 } from "../../../Data/page1";
+import { mensShoesPage1 } from "../../../Data/shoes";
 
 const HomePage = () => {
     return (
         <div>
             <MainCarousel images={mainCarouselData}/>
 
-            <div className='space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10'>
+            <div className='space-y-10 py-20 flex flex-col '>
                 <HomeSectionCarousel data={mens_kurta} sectionName={"Men's Kurta"}/>
+                <HomeSectionCarousel data={mensShoesPage1} section={"Men's Shoes"} />
                 <HomeSectionCarousel data={mens_kurta} sectionName={"Men's Kurta"}/>
-                <HomeSectionCarousel data={mens_kurta} sectionName={"Men's Kurta"}/>
-                <HomeSectionCarousel data={mens_kurta} sectionName={"Men's Kurta"}/>
-                <HomeSectionCarousel data={mens_kurta} sectionName={"Men's Kurta"}/>
+                <HomeSectionCarousel data={sareePage1} section={"Saree"}  />
             </div>
         </div>
 
