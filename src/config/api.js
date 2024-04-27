8d6c7cb5cb1719847fc26ef53bc696d8fb024303
ST-Vatsal-Shah <120327870+ST-Vatsal-Shah@users.noreply.github.com>
2024-04-27 14:59:12 +0530

    Redhjkl

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,9 @@
 
 import axios from 'axios';
-//const DEPLOYED='https://e-commerce-server-production-0873.up.railway.app'
-const LOCALHOST='http://localhost:5454'
+const DEPLOYED='https://e-commerce-server-production-0873.up.railway.app'
+//const LOCALHOST='http://localhost:5454'
 
-export const API_BASE_URL = LOCALHOST
+export const API_BASE_URL = DEPLOYED
 //'https://btep-backend-production.up.railway.app'
 
 const api = axios.create({
