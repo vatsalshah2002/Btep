aabf18c4ff12ee23e762ab36ec0d8d226c29daf2
ST-Vatsal-Shah <120327870+ST-Vatsal-Shah@users.noreply.github.com>
2024-04-28 11:03:00 +0530

    FirstCF

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,9 @@
 
 import axios from 'axios';
 const DEPLOYED='https://btep-backend-production.up.railway.app'
-//const LOCALHOST='http://localhost:5454'
+const LOCALHOST='http://localhost:5454'
 
-export const API_BASE_URL = DEPLOYED
+export const API_BASE_URL=LOCALHOST
 //'https://btep-backend-production.up.railway.app'
 
 const api = axios.create({
